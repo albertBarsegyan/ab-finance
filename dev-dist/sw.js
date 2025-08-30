1955e992fdbe2085cd9c4556a9e2601df16852b6
jack <test@gmail.com>
2025-08-30 21:28:49 +0400

    finish changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c6a197bf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ht2a6481m5g"
+    "revision": "0.dr1q4almehg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/entities/questions/ui/layout.tsx
+++ src/entities/questions/ui/layout.tsx
@@ -4,7 +4,7 @@ import { Card } from '@/shared/components/ui/card.tsx';
 export function QuestionsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex justify-center items-center min-h-[100vh] bg-background overflow-yauto">
-      <Card className="w-full max-w-md my-6">{children}</Card>
+      <Card className="w-full max-w-md my-6 mx-3">{children}</Card>
     </div>
   );
 }
